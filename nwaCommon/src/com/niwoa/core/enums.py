bd87dcb9cdd63421d40893cf4c9ccf6519a0a167
root <root@zyl>
2018-02-07 17:08:59 +0800

    init nwaCommon

---
+++ nwaCommon/src/com/niwoa/core/enums.py
@@ -0,0 +1,20 @@
+'''
+Created on 2018年2月5日
+
+@author: root
+'''
+
+from enum import Enum
+
+
+class SignEnum(Enum):
+    '''
+    classdocs
+    '''
+    colon = ":"
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ nwaCommon/src/com/niwoa/core/globals.py
@@ -0,0 +1,40 @@
+'''
+Created on 2018年2月5日
+
+@author: root
+'''
+
+import os
+
+from com.niwoa.core.enums import SignEnum
+
+
+class Globals(object):
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, mainFile, projectDepth=0):
+        '''
+        Constructor
+        '''
+        self.mainFile = mainFile
+        self.projectDepth = projectDepth
+        
+    def path(self):
+        return os.path.split(os.path.realpath(self.mainFile))[0]
+    
+    def project(self):
+        projectPathTmp = self.path()
+        count = 0;
+        while count < self.projectDepth:
+            count += 1
+            projectPathTmp = os.path.realpath(os.path.join(projectPathTmp, ".."))
+        return projectPathTmp
+    
+    def projectName(self):
+        return self.project().split(os.sep)[-1]
+        
+    def key(self, key):
+        return self.projectName() + SignEnum.colon.value + key
+    
