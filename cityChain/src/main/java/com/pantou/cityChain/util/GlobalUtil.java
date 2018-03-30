80632ed3385ead9fa83903a6f7bb136a13677cca
Administrator <Administrator@P-V-7>
2018-03-30 18:36:29 +0800

    init 6

--- cityChain/src/main/java/com/pantou/cityChain/util/GlobalUtil.java
+++ cityChain/src/main/java/com/pantou/cityChain/util/GlobalUtil.java
@@ -1,18 +1,7 @@
 package com.pantou.cityChain.util;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.util.Hashtable;
 import java.util.Random;
 import java.util.UUID;
-
-import javax.imageio.ImageIO;
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * 全局工具类
@@ -38,30 +27,4 @@ public class GlobalUtil {
 		return (T) clazz.getEnumConstants()[ordinal];
 	}
 
-	/**
-	 * 生成base64二维码
-	 */
-	public static byte[] createQrCode(String content) {
-		try {
-			Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
-			hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-			hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
-			hints.put(EncodeHintType.MARGIN, 1);
-			BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 400, 400, hints);
-			int width = bitMatrix.getWidth();
-			int height = bitMatrix.getHeight();
-			BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-			for (int x = 0; x < width; x++) {
-				for (int y = 0; y < height; y++) {
-					image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
-				}
-			}
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			ImageIO.write(image, "JPG", out);
-			return out.toByteArray();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new byte[0];
-	}
 }
